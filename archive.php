f76b47dfced7323916b6c9458069b78ea5ca50fc
Crystal Barton <cbarto11@uncc.edu>
2014-01-30 02:59:53 -0500

    fix title and section bug in archive listing.

--- archive.php
+++ archive.php
@@ -11,17 +11,28 @@ global $ns_config, $ns_template_vars;
 
 $ns_template_vars = array();
 $ns_template_vars['content-type'] = 'listing';
-if( is_day() ):
-	$ns_template_vars['page-title'] = sprintf( 'Daily Archives: %s', '<span>'.get_the_date().'</span>' );
-elseif( is_month() ):
-	$ns_template_vars['page-title'] = sprintf( 'Monthly Archives: %s', '<span>'.get_the_date('F Y').'</span>' );
-elseif( is_year() ):
-	$ns_template_vars['page-title'] = sprintf( 'Yearly Archives: %s', '<span>'.get_the_date('Y').'</span>' );
-else:
-	$ns_template_vars['page-title'] = 'Archives';
-endif;
+$post_type = get_post_type();
+$category = get_the_category();
+$tags = get_the_tags();
+$section = $ns_config->get_section( $post_type, $category, $tags, false );
 
-$ns_template_vars['section'] = $ns_config->get_default_section();
+if( $section->key == 'none' )
+{
+	if( is_day() ):
+		$ns_template_vars['page-title'] = sprintf( 'Daily Archives: %s', '<span>'.get_the_date().'</span>' );
+	elseif( is_month() ):
+		$ns_template_vars['page-title'] = sprintf( 'Monthly Archives: %s', '<span>'.get_the_date('F Y').'</span>' );
+	elseif( is_year() ):
+		$ns_template_vars['page-title'] = sprintf( 'Yearly Archives: %s', '<span>'.get_the_date('Y').'</span>' );
+	else:
+		$ns_template_vars['page-title'] = 'Archives';
+	endif;
+}
+else
+{
+	$ns_template_vars['page-title'] = $section->name;
+}
 
+$ns_template_vars['section'] = $section;
 ns_get_template_part( 'standard-template' );
 
