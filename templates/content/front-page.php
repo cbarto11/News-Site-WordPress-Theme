2005f5dce71bb14240765b32ebb913ac09c27a26
Crystal Barton <cbarto11@uncc.edu>
2014-01-30 03:14:55 -0500

    update front-page and sidebar.

--- templates/content/front-page.php
+++ templates/content/front-page.php
@@ -1,61 +1,53 @@
 <?php global $ns_config, $ns_mobile_support, $ns_template_vars, $wp_query; ?>
 <?php
-$num_columns = $ns_config->get_num_columns('front-page');
-
-//ns_print('front-page');
-//ns_print($num_columns);
+$options = $ns_config->get_admin_options( 'front-page' );
 ?>
 
 
-<?php for( $i = 0; $i < $num_columns; $i++ ): ?>
+<?php foreach( $options['sections'] as $column_name => $sections ): ?>
 
-	<div class="column <?php echo 'front-page-'.($i+1); ?>">
+	<div class="column <?php echo 'front-page-'.$column_name; ?>">
 
-	<?php $current_column = $ns_config->get_column('content', 'front-page-column-'.($i+1)); ?>
-
-	<?php if( $current_column !== null ): ?>
+	<?php
 	
-		<?php
+		foreach( $sections as $section_key ):
+	
+			$section = $ns_config->get_section_by_key( $section_key, true );
+			if( $section == null ) continue;
+		
+			$stories = $section->get_stories( 'front-page' );
 			
-			foreach( $current_column as $section_key ):
-			
-				$section = $ns_config->get_section_by_key( $section_key );
-				$stories = $section->get_stories('front-page');
+			?>
+			<div class="section-box <?php $section_key; ?>-section <?php echo $section->thumbnail_image; ?>-image">
 
-				?>
-
-				<div class="section-box <?php $section_key; ?>-section <?php echo $section->thumbnail_image; ?>-image">
-
-					<h2><?php echo $section->title; ?></h2>
-					
-					<?php
-					foreach( $stories as $story ):
-					
-						$ns_template_vars['story'] = $story;
-						$ns_template_vars['section'] = $section;
-						ns_get_template_part( 'featured', 'story', $section->key );
-					
-					endforeach;
-					?>
-					
-					<div class="more">
-						<?php echo ns_get_anchor( 
-							$section->get_section_link(), 
-							$section->name.' Archives', 
-							null,
-							'More <em>'.$section->name.'</em> &raquo;' ); ?>
-					</div><!-- .more -->
-
-				</div><!-- .section-box -->
-
+				<h2><?php echo $section->title; ?></h2>
+		
 				<?php
+				foreach( $stories as $story ):
+		
+					$ns_template_vars['story'] = $story;
+					$ns_template_vars['section'] = $section;
+					ns_get_template_part( 'featured', 'story', $section->key );
+		
+				endforeach;
+				?>
+		
+				<div class="more">
+					<?php echo ns_get_anchor( 
+						$section->get_section_link(), 
+						$section->name.' Archives', 
+						null,
+						'More <em>'.$section->name.'</em> &raquo;' ); ?>
+				</div><!-- .more -->
 
-			endforeach; // foreach( $current_column as $section_key )
-		?>
-	
-	<?php endif; // if( count($columns) > $i ) ?>
-	
+			</div><!-- .section-box -->
+			<?php
+
+		endforeach; // foreach( $current_column as $section_key )
+	?>
+
+	<?php ns_use_widget( 'content', 'bottom' ); ?>
 	</div><!-- .column -->
 
-<?php endfor; ?>
+<?php endforeach; // foreach( $options['sections'] as $column_name => $sections ) ?>
 
--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,10 +1,6 @@
-
-
 <?php global $ns_config, $ns_mobile_support, $ns_template_vars; ?>
-
-
 <?php
-$column = $ns_config->get_value('sidebar', 'sidebar-column-1');
+$options = $ns_config->get_admin_options( 'sidebar' );
 ?>
 
 
@@ -13,48 +9,53 @@ $column = $ns_config->get_value('sidebar', 'sidebar-column-1');
 	<div id="sidebar" class="clearfix">
 	<?php ns_use_widget( 'sidebar', 'top' ); ?>
 
-		<div class="column column-sidebar">
+	<?php foreach( $options['sections'] as $column_name => $sections ): ?>
+
+		<div class="column <?php echo 'front-page-'.$column_name; ?>">
 
 		<?php
-		foreach( $column as $section_key ):
-	
-			$section = $ns_config->get_section_by_key( $section_key );
-			$stories = $section->get_stories('front-page');
-
-			?>
-
-			<div class="section-box <?php $section_key; ?>-section <?php echo $section->thumbnail_image; ?>-image">
-
-				<h2><?php echo $section->title; ?></h2>
-			
-				<?php
-				global $ns_story;
-				foreach( $stories as $story ):
-			
-					$ns_story = $story;
-					ns_get_template_part( 'featured', 'story', $ns_section->key );
-			
-				endforeach;
-				?>
-			
-				<div class="more">
-					<?php echo ns_get_anchor( 
-						$section->get_section_link(), 
-						$section->name.' Archives', 
-						null,
-						'More <em>'.$section->name.'</em> &raquo;' ); ?>
-				</div><!-- .more -->
-
-			</div><!-- .section-box -->
-
-			<?php
-
-		endforeach; // foreach( $column as $section_key )
-		?>
 		
+			foreach( $sections as $section_key ):
+		
+				$section = $ns_config->get_section_by_key( $section_key, true );
+				if( $section == null ) continue;
+			
+				$stories = $section->get_stories( 'sidebar' );
+				?>
+				<div class="section-box <?php $section_key; ?>-section <?php echo $section->thumbnail_image; ?>-image">
+
+					<h2><?php echo $section->title; ?></h2>
+			
+					<?php
+					global $ns_story;
+					foreach( $stories as $story ):
+			
+						$ns_template_vars['story'] = $story;
+						$ns_template_vars['section'] = $section;
+						ns_get_template_part( 'featured', 'story', $section->key );
+			
+					endforeach;
+					?>
+			
+					<div class="more">
+						<?php echo ns_get_anchor( 
+							$section->get_section_link(), 
+							$section->name.' Archives', 
+							null,
+							'More <em>'.$section->name.'</em> &raquo;' ); ?>
+					</div><!-- .more -->
+
+				</div><!-- .section-box -->
+				<?php
+
+			endforeach; // foreach( $current_column as $section_key )
+		?>
+	
+		<?php ns_use_widget( 'sidebar', 'bottom' ); ?>
 		</div><!-- .column -->
 
-	<?php ns_use_widget( 'sidebar', 'bottom' ); ?>
+	<?php endforeach; // foreach( $options['sections'] as $column_name => $sections ) ?>
+	
 	</div><!-- #sidebar -->
 	
 </div><!-- #sidebar-wrapper -->
