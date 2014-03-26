0fae70f6cafc568903fc33943292a92863a5adbd
Crystal Barton <cbarto11@uncc.edu>
2014-03-26 08:19:41 -0400

    alter search value to fix bug that appears on '/search/<term>' pages.

--- templates/content/search.php
+++ templates/content/search.php
@@ -17,7 +17,7 @@ else:
 	<p>Sorry, but nothing matched your search criteria. Please try again with some different keywords.</p>
 	<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>" >
 		<label class="screen-reader-text" for="s">Search for:</label>
-		<div class="textbox_wrapper"><input type="text" value="<?php echo $_GET['s']; ?>" name="s" id="s" /></div>
+		<div class="textbox_wrapper"><input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" /></div>
 		<input type="submit" id="searchsubmit" value="Search" />
 	</form>
 	<?php
